cb1acd312b188de79bbd7732b9dd487a264ebe8e
Diarilala <hei.diarilala@gmail.com>
2025-08-04 17:10:48 +0300

    Fetching and delimiting pokedex size

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const sprite = jsonVersion.sprites.font_default;
+
+  //fetching pokemon
+  useEffect(() => {
+    const fetching = async () => {
+      try {
+        const fetchedData = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const jsonVersion = await fetchedData.json();
+        pokemonWithIds = jsonVersion.map((pokemon, index) => ({
+          ...pokemon,
+          id: index + 1
+        }));
+        setLoading(false);
+        setPokemonList(pokemonWithIds);
+      } catch (error) {
+        console.log("Error fetching Pokemon:", error);
+        setLoading(false);
+      }
+    };
+
+    fetching();
+  }, []);
+  
+function display(){
+  console.log(pokemonList)
+}
+display();
 
   return (
-    <>
-      
-    </>
+    <div className='w-sm h-[90vh] bg-white'>
+      <header className='w-full h-fit bg-amber-500 border-b-1 border-black'>
+        <div className='flex'>
+          <h2 className='font-VT my-1 ml-2 text-xl rounded-xl bg-orange-300 text-white px-4 border-1 border-black mb- '><b>POKEDEX NATIONAL</b></h2>
+        </div>
+      </header>
+    </div>
   )
 }
 
